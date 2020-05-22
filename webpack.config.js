01032b8fd2e2c087588e760caf8f73f7fa84521d
Nikita Inkin <nikitainkin@yahoo.com>
2020-05-22 23:12:55 +0300

    added file and image loader config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash'); // добавили плагин
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -22,7 +23,24 @@ module.exports = {
       {
         test: /\.css$/,
         use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-    }
+    },
+    {
+      test: /\.(png|jpg|gif|ico|svg|)$/,
+      use: [
+          'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения?name=../images/[name].[ext]
+          {
+              loader: 'image-webpack-loader',
+              options: {
+                  esModule: false
+              }
+          },
+      ]
+  },
+  {
+    test: /\.(eot|ttf|woff|woff2)$/,
+    loader: 'file-loader?name=./vendor/[name].[ext]'
+
+}
     ]
   },
   plugins: [ 
@@ -34,6 +52,14 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html'
     }),
-    new WebpackMd5Hash()
+    new WebpackMd5Hash(),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true,
+    }),
   ]
 };
