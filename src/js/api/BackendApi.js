a601e2970f9290f2bbcad834f1565adb4d1060cc
Nikita Inkin <nikitainkin@yahoo.com>
2020-06-21 23:18:20 +0300

    initial structure

---
+++ src/js/api/BackendApi.js
@@ -0,0 +1,25 @@
+export default class BackendApi {
+    constructor(apiData) {
+        this.apiData = apiData;
+    }
+
+    signUp(email, password, name) {
+        
+    }
+    
+    signIn(email, password) {
+
+    }
+
+    getUser() {
+
+    }
+
+    getSavedArticles() {
+
+    }
+
+    deleteCard(cardId){
+        
+    }
+}
